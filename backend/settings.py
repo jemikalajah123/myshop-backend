1a369047a49a2dd5f6d1f3f9e15ef07c9ed0a06a
jemikalajah123 <okeoghene.philip5@gmail.com>
2021-06-07 14:59:18 +0100

    first-version

--- backend/settings.py
+++ backend/settings.py
@@ -171,7 +171,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 CORS_ALLOWED_ORIGINS = [
     "https://example.com",
-    "https://sub.example.com",
+    "https://shopflex.netlify.app",
     "http://localhost:3000",
     "http://127.0.0.1:9000"
 ]
